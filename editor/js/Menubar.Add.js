77a84a88e45bf9c6888222fbcd18d7e367eeb10e
Deba <aldealdo@gmail.com>
2018-01-11 09:10:39 +0100

    Fixed absolute path for Adding MOD

--- editor/js/Menubar.Add.js
+++ editor/js/Menubar.Add.js
@@ -62,7 +62,7 @@ Menubar.Add = function ( editor ) {
 		// load a resource
 		loader.load(
 			// resource URL
-			'/MODs.json',
+			'../MODs.json',
 
 			function ( obj ) {
 				console.log(meshCount);
